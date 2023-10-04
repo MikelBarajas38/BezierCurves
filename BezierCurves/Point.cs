986e91ae752b8d62f77cb15d3c27dbbdff2d7710
Luis <luisangelsm04@hotmail.com>
2023-10-03 18:52:18 -0600

    Function CalculateBezierPoint added

--- BezierCurves/Point.cs
+++ BezierCurves/Point.cs
@@ -11,5 +11,13 @@ namespace BezierCurves
         public double x;
         public double y;
         public double z;
+
+        public Point(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
     }
 }
--- BezierCurves/Program.cs
+++ BezierCurves/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BezierCurves;
+
 Console.WriteLine("Hello, World!");
+
+Point CalculateBezierPoint(double t, Point p0, Point p1, Point p2, Point p3)
+{
+    double u = 1 - t;
+    double tt = t * t;
+    double uu = u * u;
+    double uuu = uu * u;
+    double ttt = tt * t;
+
+    double x = (int)(p0.x * uuu + 3 * p1.x * uu * t + 3 * p2.x * u * tt + p3.x * ttt);
+    double y = (int)(p0.y * uuu + 3 * p1.y * uu * t + 3 * p2.y * u * tt + p3.y * ttt);
+    double z = (int)(p0.z * uuu + 3 * p1.z * uu * t + 3 * p2.z * u * tt + p3.z * ttt);
+
+    return new Point(x, y, z);
+}
